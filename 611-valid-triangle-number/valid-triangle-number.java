a19964112f90ad8f4c958932595318f012b8f470
Utkarsh449 <146366611+Utkarsh449@users.noreply.github.com>
2026-01-23 22:08:28 +0530

    Time: 6 ms (99.95%) | Memory: 46.2 MB (13.93%) - LeetSync

---
+++ 611-valid-triangle-number/valid-triangle-number.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int triangleNumber(int[] nums) {
+        int n;
+        int max =0;
+        int[] count = new int[1001];
+        for(int i : nums){
+            count[i]++;
+            max = Math.max(max,i);
+        }
+        count[0] =0;
+        int idx =0;
+        for(int i=1;i<=max;i++){
+            for(int j=0;j<count[i];++j,++idx){
+                nums[idx] = i;
+            }
+            count[i] += count[i-1];
+        }
+        n = idx;
+        int r = 0;
+        for(int i=0;i<n-2;i++){
+            for(int j=i+1;j<n-1;j++){
+                if(nums[i] + nums[j] >max){
+                    r += (n-j)*(n-j-1)/2;
+                    break;
+                }
+                r += count[nums[i] + nums[j] -1]-j-1;
+            }
+        }
+        return r;
+    }
+}
