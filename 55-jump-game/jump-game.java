9002bd71e51ba9b8942dff86a792e1ea14991002
Utkarsh449 <146366611+Utkarsh449@users.noreply.github.com>
2026-02-02 16:52:54 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (7.17%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    static {
+        Runtime.getRuntime().gc();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try (FileWriter f = new FileWriter("display_runtime.txt")) {
+                f.write("0");
+            } catch (Exception e) {
+
+            }
+        }));
+    }
+
+    public boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+
+        for(int i=nums.length - 1;i>=0;i--){
+            if(nums[i] + i >= goal){
+                goal = i;
+            }
+        }
+
+        return goal == 0;
+    }
+}
