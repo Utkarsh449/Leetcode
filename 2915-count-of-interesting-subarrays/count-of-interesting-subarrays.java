e8df0b1f0390a2ac9f50551f4b1a538cb1947927
Utkarsh449 <146366611+Utkarsh449@users.noreply.github.com>
2026-02-21 13:55:52 +0530

    Time: 6 ms (100.00%) | Memory: 104.9 MB (98.50%) - LeetSync

--- 2915-count-of-interesting-subarrays/count-of-interesting-subarrays.java
+++ 2915-count-of-interesting-subarrays/count-of-interesting-subarrays.java
@@ -1,22 +1,21 @@
-import java.util.*;
-
 class Solution {
     public long countInterestingSubarrays(List<Integer> nums, int modulo, int k) {
-        long ans = 0;
-        Map<Integer, Long> freq = new HashMap<>();
-        freq.put(0, 1L); // prefix sum 0 initially occurs once
+        int N = nums.size();
+        if ( modulo == 1 ) return (long)N * (N+1) / 2;
+        int[] sum = new int[N+1];
 
-        long count = 0;
-        for (int x : nums) {
-            if (x % modulo == k) {
-                count++;
-            }
-            int rem = (int)(count % modulo);
-            int target = (rem - k + modulo) % modulo;
-            
-            ans += freq.getOrDefault(target, 0L);
-            freq.put(rem, freq.getOrDefault(rem, 0L) + 1);
+        for ( int ii = 0; ii < N; ii++ ) {
+            sum[ii+1] = sum[ii] + (nums.get(ii) % modulo == k ? 1 : 0);
         }
-        return ans;
+
+        long result = 0;
+
+        int[] cnt = new int[Math.min(modulo,N+1)];
+        for ( int ss : sum ) {
+            if ( ss >= k ) result += cnt[(ss-k)%modulo];
+            cnt[ss%modulo]++;
+        }
+
+        return result;
     }
 }
